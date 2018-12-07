6f0f3355fdd1db751e2b50bb46143e2198c65e41
bluburry <43838339+bluburry@users.noreply.github.com>
2018-12-06 22:32:12 -0600

    Update controller.php

--- server/controller.php
+++ server/controller.php
@@ -1,13 +1,17 @@
 <?php
 include 'connection.php';
+include 'sanitization.php';
 session_start(); //start the session
 $result = "";
-$request_type="search";
+//search_cars($connection);
+//return_car($connection);
+
 //only process the data if there a request was made and the session is active
 if (isset($_POST["type"])) {
     session_regenerate_id(); //regenerate the session to prevent fixation
     $_SESSION['start'] = time(); //reset the session start time
-    $request_type=$_POST["type"];
+    $request_type = sanitizeMYSQL($connection, $_POST['type']);
+
 
     switch ($request_type) { //check the request type
         case "search":
@@ -20,41 +24,44 @@ if (isset($_POST["type"])) {
             $result = get_history($connection);
             break;
         case "rent":
-            $result=drop($connection,$_POST['rent_id']);
+            $result=drop($connection);
             break;
         case "return":
-            $result=drop($connection,$_POST['return_id']);
+            $result=drop($connection);
             break;
     }
 }
     function search_cars($connection) {
-
-
+    if (isset($_POST['search']) && trim($_POST['search']) != "") {
+    $data = sanitizeMYSQL($connection, $_POST['search']);
+    }
     $final = array();
-    $final["search"] = array();
-    $query = "SELECT * FROM carspecs";
+    $final["search_car"] = array();
+    $query = "SELECT cs.make,cs.yearmade, c.id,cs.model,c.picture_type, c.picture,cs.size,c.picture,c.picture_type,c.color,c.status FROM carspecs cs inner join car c WHERE cs.make LIKE '$data' OR cs.yearmade LIKE '$data' OR cs.model LIKE '$data' or cs.size like '$data' or c.color like '$data'";
     $result = mysqli_query($connection, $query);
-    if ($result)
+    if (!$result)
         return json_encode($array);
     else {
         $row_count = mysqli_num_rows($result);
         for ($i = 0; $i < $row_count; $i++) {
             $row = mysqli_fetch_array($result);
-            $array["picture"] = 'data:' . $row["Picture_Type"] . ';base64,' . base64_encode($row["Picture"]);
-            $array["make"] = $row["FirstName"] . " " . $row["LastName"];
-            $array["model"] = $row["Gender"] == 'M' ? "Male" : "Female";
-            $array["year"] = date_diff(date_create($row["DateOfBirth"]), date_create('now'))->y;
-            $array["color"] = $row["color"];
+            $array = array();
+            //$array["picture"] = 'data:' . $row["Picture_type"] . ';base64,' . base64_encode($row["Picture"]);
+            $array["make"] = $row["make"];
+            $array["picture"] = 'data:' . $row["picture_type"] . ';base64,' . base64_encode($row["picture"]);
+            $array["model"] = $row["model"];
+            $array["year"] = $row["yearmade"];
             $array["size"] = $row["size"];
-            $array["id"] = $row["rentalid"];
-            $final["search"][] = $array;
+            $array["ID"] = $row["id"];
+            $array["color"] = $row["color"];
+            $final["search_car"][] = $array;
         }
     }
     return json_encode($final);
 }
 
 function rent_car($connection) {
-	$date= date_create('Y-m-d');
+    $date= date_create('Y-m-d');
     $id=$_POST['id'];
     $query1 = "UPDATE rental SET status= '2', rentDate='$date' WHERE status='1' AND carID='$id'";
         $result1 = mysqli_query($connection, $query1);
@@ -89,7 +96,7 @@ function return_car($connection) {
     $final = array();
     $final["rented_car"] = array();
     //write a query about the enrolled courses for that student. The student ID is from the session
-    $query = "select cs.id, cs.make,yearmade,cs.model,cs.size,c.status, cm.`Name`, r.`ID` as rentalid, r.`rentdate`
+    $query = "select cs.id, c.Picture, c.picture_type, cs.make,yearmade,cs.model,cs.size,c.status, cm.`Name`, r.`ID` as rentalid, r.`rentdate`
                 from carspecs cs
                 inner join car c on c.`CarSpecsID`=cs.`ID`
                 inner join rental r on r.`carID`=c.id
@@ -106,6 +113,7 @@ function return_car($connection) {
             $array = array();
             $array["make"] = $row["make"];
             $array["model"] = $row["model"];
+            $array["picture"] = 'data:' . $row["Picture_type"] . ';base64,' . base64_encode($row["Picture"]);
             $array["year"] = $row["yearmade"];
             $array["size"] = $row["size"];
             $array["rental_ID"] = $row["rentalid"];
@@ -118,7 +126,7 @@ function return_car($connection) {
 function get_history($connection) {
     $final = array();
     $final["returned_car"] = array();
-    $query = "select cs.id, cs.make,yearmade,cs.model,cs.size,c.status,  r.`ID` as rentalid, r.`returndate`
+    $query = "select cs.id,c.Picture, c.picture_type, cs.make,yearmade,cs.model,cs.size,c.status,  r.`ID` as rentalid, r.`returndate`
                 from carspecs cs
                inner join car c on c.`CarSpecsID`=cs.`ID`
                 inner join rental r on r.`carID`=c.id
@@ -136,10 +144,11 @@ function get_history($connection) {
             $array = array();
             $array["make"] = $row["make"];
             $array["model"] = $row["model"];
+            $array["picture"] = 'data:' . $row["Picture_type"] . ';base64,' . base64_encode($row["Picture"]);
             $array["year"] = $row["yearmade"];
             $array["size"] = $row["size"];
             $array["rental_ID"] = $row["rentalid"];
-            $array["rent_date"] = $row["returndate"];
+            $array["return_date"] = $row["returndate"];
             $final["returned_car"][] = $array;
         }
     }
@@ -147,3 +156,4 @@ function get_history($connection) {
 }
 
 echo $result;
+
